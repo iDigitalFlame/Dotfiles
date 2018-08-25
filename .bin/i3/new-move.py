50a3ecc15ad29d4c1d5a329165451100d2d17574
iDigitalFlame <idf@idigitalflame.com>
2018-08-25 14:25:27 -0400

    Updates and switch to black

--- .bin/i3/new-move.py
+++ .bin/i3/new-move.py
@@ -20,7 +20,7 @@
 ## New Workspace with Move
 # iDigitalFlame 2018
 
-from sys import exit, path
+from sys import exit, path, argv
 from os.path import dirname, abspath
 
 path.insert(0, dirname(abspath(__file__)))
@@ -30,29 +30,45 @@ from new import i3_command, i3_output
 
 def i3_get_focused(i3_node=None):
     if i3_node is None:
-        i3_node = i3_command('get_tree')
-    if i3_node is not None and 'nodes' in i3_node:
-        for node in i3_node['nodes']:
-            if 'type' in node and node['type'] == 'con':
-                if 'name' in node and node['name'] is not None and 'focused' in node and node['focused']:
-                    if 'id' in node:
-                        return node['id']
-            if 'type' in node and 'nodes' in node:
+        i3_node = i3_command("get_tree")
+    if i3_node is not None and "nodes" in i3_node:
+        for node in i3_node["nodes"]:
+            if "type" in node and node["type"] == "con":
+                if (
+                    "name" in node
+                    and node["name"] is not None
+                    and "focused" in node
+                    and node["focused"]
+                ):
+                    if "id" in node:
+                        return node["id"]
+            if "type" in node and "nodes" in node:
                 node_result = i3_get_focused(node)
                 if node_result is not None:
                     return node_result
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     i3_focused = i3_get_focused()
-    i3_workspaces = i3_command('get_workspaces')
+    i3_workspaces = i3_command("get_workspaces")
     if i3_workspaces is not None and i3_focused is not None:
-        i3_next_empty = next(x for x in range(1, 50) if not [w for w in i3_workspaces if w['num'] == x])
-        i3_output(['[con_id="%s"]' % i3_focused, 'move', 'container', 'to', 'workspace', str(i3_next_empty)])
-        if len(sys.argv) > 1:
-            i3_output(['workspace', str(i3_next_empty)])
-        sys.exit(0)
-    sys.exit(1)
+        i3_next_empty = next(
+            x for x in range(1, 50) if not [w for w in i3_workspaces if w["num"] == x]
+        )
+        i3_output(
+            [
+                '[con_id="%s"]' % i3_focused,
+                "move",
+                "container",
+                "to",
+                "workspace",
+                str(i3_next_empty),
+            ]
+        )
+        if len(argv) > 1:
+            i3_output(["workspace", str(i3_next_empty)])
+        exit(0)
+    exit(1)
 
 # EOF
--- .bin/i3/new.py
+++ .bin/i3/new.py
@@ -26,22 +26,24 @@ from subprocess import Popen, PIPE, DEVNULL
 
 
 def i3_command(i3_type):
-    return i3_output(['-t', i3_type])
+    return i3_output(["-t", i3_type])
 
 
 def i3_output(i3_command):
     i3_exec_params = i3_command
     if not isinstance(i3_command, list):
-        i3_exec_params = str(i3_command).split(' ')
+        i3_exec_params = str(i3_command).split(" ")
     try:
-        i3_exec = Popen(['/usr/bin/i3-msg'] + i3_exec_params, stdout=PIPE, stderr=DEVNULL)
+        i3_exec = Popen(
+            ["/usr/bin/i3-msg"] + i3_exec_params, stdout=PIPE, stderr=DEVNULL
+        )
         if i3_exec.wait() != 0:
             print('[!] "i3-msg" returned non-zero status!')
-            sys.exit(1)
+            exit(1)
         i3_output = i3_exec.stdout.read()
         if len(i3_output) > 0:
             try:
-                i3_json = loads(i3_output.decode('UTF-8'))
+                i3_json = loads(i3_output.decode("UTF-8"))
                 return i3_json
             except JSONDecodeError as err:
                 print('[!] "i3-msg" output was not in JSON format! (%s)' % str(err))
@@ -55,11 +57,13 @@ def i3_output(i3_command):
         exit(1)
 
 
-if __name__ == '__main__':
-    i3_workspaces = i3_command('get_workspaces')
+if __name__ == "__main__":
+    i3_workspaces = i3_command("get_workspaces")
     if i3_workspaces is not None:
-        i3_next_empty = next(x for x in range(1, 50) if not [w for w in i3_workspaces if w['num'] == x])
-        i3_output(['workspace', 'number', str(i3_next_empty)])
+        i3_next_empty = next(
+            x for x in range(1, 50) if not [w for w in i3_workspaces if w["num"] == x]
+        )
+        i3_output(["workspace", "number", str(i3_next_empty)])
         exit(0)
     exit(1)
 
